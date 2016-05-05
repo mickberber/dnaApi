568879765ff1332a87c168e081b9a9c6c306cac4
Michael Berber <mickberber@icloud.com>
2016-05-05 13:04:41 -0700

    allow cross origin

--- server.js
+++ server.js
@@ -3,7 +3,20 @@ var app = express();
 var path = require('path');
 var port = process.env.PORT || 8080;
 var data = require('./db.js');
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 
 app.get('/', function(req, res) {
   var dnaData = data.retrieveOne();
